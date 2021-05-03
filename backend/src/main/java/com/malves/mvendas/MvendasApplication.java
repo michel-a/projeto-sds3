7e7806d4cc63fbe5dbbc7acc3ff28788e5e7144c
Michel Alves <malvesdesign@hotmail.com>
2021-05-03 15:38:53 -0300

    Project created

---
+++ backend/src/main/java/com/malves/mvendas/MvendasApplication.java
@@ -0,0 +1,13 @@
+package com.malves.mvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/malves/mvendas/MvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malves.mvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
