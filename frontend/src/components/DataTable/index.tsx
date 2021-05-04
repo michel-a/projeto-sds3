0e1fb884bd57baf0a7ea4809ce1b18d0b92ea62a
Michel Alves <malvesdesign@hotmail.com>
2021-05-04 19:16:58 -0300

    Background change

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,7 +1,7 @@
 const DataTable = () => {
     return (
         <div className = "table-responsive" >
-            <table className="table table-striped table-sm">
+            <table className="table table-striped table-sm table-hover">
                 <thead>
                     <tr>
                         <th>Data</th>
@@ -12,20 +12,6 @@ const DataTable = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
                     <tr>
                         <td>22/04/2021</td>
                         <td>Barry Allen</td>
